a9bdadb490c2c7eb9d8d718728da45c3655e752d
tulika-mall <88815712+tulika-mall@users.noreply.github.com>
2024-06-22 13:20:28 +0530

    minor changes

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -80,7 +80,7 @@ const NewsCards = () => {
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
             <div className="text-center">
               <h1 className="text-4xl sm:text-6xl font-bold text-gray-800 dark:text-gray-200">
-                Newsly
+                Newsway
               </h1>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
                 Your Daily Dose of Headlines Delivered Daily.
